37cb42d889cca0bb64cf01c6eade675a29b3b6af
amgillenwater <amgillenwater@gmail.com>
2020-07-05 12:14:25 -0400

    converted cardfeed to function w/ hooks

--- social-cards/src/components/cardFeed.js
+++ social-cards/src/components/cardFeed.js
@@ -1,38 +1,25 @@
-import React from 'react'
+/* globals localStorage */
+import React, { useState, useEffect } from 'react'
 import { getCards } from '../api'
 
-class CardFeed extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      cards: [],
-      token: window.localStorage.getItem('login_auth_token')
-    }
-  }
+const CardFeed = (props) => {
+  const [cards, setCards] = useState([])
 
-  componentDidMount () {
-    if (this.state.token) {
-      getCards(this.state.token)
-        .then(cards => this.setState({ cards: cards }))
+  useEffect(() => {
+    if (localStorage.getItem('login_auth_token')) {
+      getCards(localStorage.getItem('login_auth_token')).then(cards => setCards(cards))
     }
-  }
+  }, [])
 
-  componentDidUpdate (prevProps, prevState) {
-    if (this.state.token && this.state.token !== prevState.token) {
-      getCards(this.state.token).then(cards => this.setState({ cards: cards }))
-    }
-  }
-
-  render () {
-    const card = this.state.cards.map((cards, idx) =>
-      <li key={cards.idx}>{cards.author}</li>
-    )
-    return (
-      <div className='CardFeed'>
-        <p>{card}</p>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <ul>
+        {cards && cards.map((card) => {
+          return (
+            <li key={card.id}>{card.author}</li>)
+        })}
+      </ul>
+    </div>)
 }
 
 export default CardFeed
