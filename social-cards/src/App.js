7b87d205c18f38ee5166ce442115913014650211
amgillenwater <amgillenwater@gmail.com>
2020-07-07 01:24:32 -0400

    added grommet

--- social-cards/src/App.js
+++ social-cards/src/App.js
@@ -1,26 +1,99 @@
 /* globals localStorage */
 import React, { useState } from 'react'
-import './App.css'
 import LogIn from './components/login'
 import CardFeed from './components/CardFeed'
 import CardCreator from './components/CardCreator'
-import Home from './components/Home'
+import { Box, Button, Collapsible, Heading, Grommet, Layer, ResponsiveContext } from 'grommet'
+import { FormClose, Menu } from 'grommet-icons'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+const theme = {
+  colors: {
+    brand: '#228BE6'
+  },
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px'
+    }
+  }
+}
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+)
 function App (props) {
   const [token, setToken] = useState(localStorage.getItem('login_auth_token'))
   // ^ if local storage has a token, it will have that in state, otherwise null/undef
-
+  const [showSidebar, setShowSidebar] = useState(false)
   return (
-    <div className='App'>
-      <Router>
-        <Switch>
-          <Route path='/' exact render={() => <LogIn setToken={setToken} />} />
-          <Route path='/feed' render={() => <CardFeed token={token} />} />
-          <Route path='/new' render={() => <CardCreator token={token} />} />
-        </Switch>
-      </Router>
-    </div>
+    <Grommet theme={theme} full>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar>
+              <Heading level='3' margin='none'>Card Club</Heading>
+              <Button
+                icon={<Menu />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+              <Box flex align='center' justify='center'>
+                <LogIn setToken={setToken} />
+              </Box>
+              {(!showSidebar || size !== 'small') ? (
+                <Collapsible direction='horizontal' open={showSidebar}>
+                  <Box
+                    flex
+                    width='medium'
+                    background='light-2'
+                    elevation='small'
+                    align='center'
+                    justify='center'
+                  >
+                      sidebar
+                  </Box>
+                </Collapsible>
+              ) : (
+                <Layer>
+                  <Box
+                    background='light-2'
+                    tag='header'
+                    justify='end'
+                    align='center'
+                    direction='row'
+                  >
+                    <Button
+                      icon={<FormClose />}
+                      onClick={() => setShowSidebar(!showSidebar)}
+                    />
+                  </Box>
+                  <Box
+                    fill
+                    background='light-2'
+                    align='center'
+                    justify='center'
+                  >
+                      sidebar
+                  </Box>
+                </Layer>
+              )}
+            </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
   )
 }
 
